63ecd55a03fe3d8802c42021bf84bb1dd57a8e26
iGoodsoul <goodsoulone@gmail.com>
2022-01-12 17:41:29 +0300

    realization Smartphone

--- src/com/sales/Main.java
+++ src/com/sales/Main.java
@@ -1,10 +1,12 @@
 package com.sales;
 
+import com.sales.goods.electronic.Electronic;
+import com.sales.goods.electronic.Laptop;
 import com.sales.goods.medical.Drug;
 
 public class Main {
 
     public static void main(String[] args) {
-
+    
     }
 }
--- src/com/sales/goods/electronic/Smartphone.java
+++ src/com/sales/goods/electronic/Smartphone.java
@@ -3,8 +3,20 @@ package com.sales.goods.electronic;
 //TODO: finish
 public class Smartphone extends Electronic{
 
-
-    public Smartphone(long price, String description, Brand brand) {
+    byte numberOfButtons;
+    public Smartphone(long price, String description, Brand brand, byte numberOfButtons) {
         super(price, description, brand);
+        this.numberOfButtons = numberOfButtons;
+    }
+
+    public byte getNumberOfButtons() {
+        return numberOfButtons;
+    }
+
+    @Override
+    public String toString() {
+        return "com.sales.goods.electronic.Smartphone{" +
+                "numberOfButtons=" + numberOfButtons +
+                '}';
     }
 }
