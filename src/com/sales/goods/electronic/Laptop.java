6bfa1f655c5adb0fc4e23e644dc05de102a6bd7e
iGoodsoul <goodsoulone@gmail.com>
2022-01-12 17:28:57 +0300

    realization Laptop

--- src/com/sales/goods/electronic/Laptop.java
+++ src/com/sales/goods/electronic/Laptop.java
@@ -3,8 +3,21 @@ package com.sales.goods.electronic;
 //TODO: finish
 public class Laptop extends Electronic {
 
-    public Laptop(long price, String description, Brand brand) {
+
+    byte displaySize;
+    public Laptop(long price, String description, Brand brand, byte displaySize) {
         super(price, description, brand);
+        this.displaySize = displaySize;
     }
 
+    public byte getDisplaySize() {
+        return displaySize;
+    }
+
+    @Override
+    public String toString() {
+        return "com.sales.goods.electronic.Laptop{" +
+                "displaySize=" + displaySize +
+                '}';
+    }
 }
